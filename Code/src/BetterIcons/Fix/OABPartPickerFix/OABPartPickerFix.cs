78e0b445dc03a52807953550d5ea5b9c12031247
pasalvetti <pasalvetti@gmail.com>
2024-09-16 00:19:40 +0200

    Refactor + add engines icon support

--- Code/src/BetterIcons/Fix/OABPartPickerFix/OABPartPickerFix.cs
+++ Code/src/BetterIcons/Fix/OABPartPickerFix/OABPartPickerFix.cs
@@ -22,15 +22,13 @@ namespace BetterIcons.Fix.OABPartPickerFix
             HarmonyInstance.PatchAll(typeof(OABPartPickerFix));
         }
 
-        private static void changeIcon(String partName)
+        private static void changeIcon(String objectName, String imageName)
         {
-            GameObject gameObject = GameObject.Find(pathToPartsPicker + "BTN-" + partName + "/");
-            Image image = gameObject.GetChild("ICO-Category-Placeholder").GetComponent<Image>();
-            Debug.Log(partName + " image has been found: " + image.name + " of type " + image.GetType());
-            //int width = 48;
-            //int height = 48;
+            GameObject gameObject = GameObject.Find(pathToPartsPicker + objectName + "/");
+            Image image = gameObject.GetChild(imageName).GetComponent<Image>();
+            Debug.Log(imageName + " image has been found: " + image.name);
             Texture2D texture2D = new Texture2D(width, height);
-            texture2D.LoadImage(File.ReadAllBytes("./BepInEx/plugins/BetterIcons/PartCategories-ICO-" + partName + ".png"));
+            texture2D.LoadImage(File.ReadAllBytes("./BepInEx/plugins/BetterIcons/" + imageName + ".png"));
             image.sprite = Sprite.Create(texture2D, new Rect(0.0f, 0.0f, (float)width, (float)height), new Vector2(0.0f, 0.0f));
             List<GameObject> children = gameObject.GetAllChildren();
             foreach (GameObject child in children)
@@ -43,21 +41,10 @@ namespace BetterIcons.Fix.OABPartPickerFix
         [HarmonyPostfix]
         public static void StartPostfix()
         {
-            GameObject gameObject = GameObject.Find("OAB(Clone)/HUDSpawner/HUD/widget_PartsPicker/mask_PartsPicker/GRP-Body/GRP-Part-Categories/BTN-Favorites/");
-            Image image = gameObject.GetChild("icon_favorites").GetComponent<Image>();
-            Debug.Log("Favorite image has been found: "+ image.name+" of type "+ image.GetType());
-            int width = 24;
-            int height = 24;
-            Texture2D texture2D = new Texture2D(width, height);
-            texture2D.LoadImage(File.ReadAllBytes("./BepInEx/plugins/BetterIcons/icon.png"));
-            image.sprite = Sprite.Create(texture2D, new Rect(0.0f, 0.0f, (float)width, (float)height), new Vector2(0.0f, 0.0f));
-            List<GameObject> children = gameObject.GetAllChildren();
-            foreach (GameObject child in children)
-            {
-                Debug.Log("child: " + child.name);
-            }
-            changeIcon("Pods");
-            changeIcon("FuelTanks");
+            changeIcon("BTN-Favorites", "icon_favorites");
+            changeIcon("BTN-Pods", "ICO-Category-Placeholder");
+            changeIcon("BTN-Fuel", "icon_fuel");
+            changeIcon("BTN-Engines", "icon_engines");
         }
 
     }
